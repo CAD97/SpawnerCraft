8f6e7e6ff707aa8072b362307adb2105bd5f0efd
CAD97 <cad97@cad97.com>
2016-10-09 01:55:32 -0400

    Update forge version to most recent
    I want to use @Mod.EventBusSubscriber, but I can't figure it out right now

--- src/main/java/cad97/spawnercraft/SpawnerCraft.java
+++ src/main/java/cad97/spawnercraft/SpawnerCraft.java
@@ -18,7 +18,7 @@ public class SpawnerCraft {
 
     static final String VERSION = "3.1";
     static final String GUI_FACTORY_CLASS = "cad97.spawnercraft.client.gui.GuiFactory"; // GuiFactory.class.getCanonicalName()
-    static final String DEPENDENCIES = "required-after:Forge@[12.18.0.2002,);";
+    static final String DEPENDENCIES = "required-after:Forge@[12.18.2.2097,);";
 
     @Mod.Instance
     static SpawnerCraft instance;
